44d4e68137a795e58dc6d7a72f27475e672253dd
angelgm1729 <angel.granado@correo.unimet.edu.ve>
2023-06-04 03:46:54 -0400

    Setting some pages :building_construction:

---
+++ src/constants/urls.js
@@ -0,0 +1,5 @@
+// Links de todas las rutas de la aplicación
+
+export const HOME_URL = "/";
+export const LOGIN_URL = "/login";
+export const REGISTER_URL = "/register";
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./LoginPage.module.css";
+
+export function LoginPage() {
+    return (
+        <div>
+            <h1>LoginPage</h1>
+        </div>
+    )
+}
