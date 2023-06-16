56d770992eb3b55563269c7f159be034e25ac753
BeaCardozo <113352853+BeaCardozo@users.noreply.github.com>
2023-06-16 11:48:11 -0400

    objetivos, mision, vision pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import './App.css'
 import Navbar from "./components/NavBar/Navbar"
 import Footer from "./components/Footer/Footer"
 import LandingPage from "./pages/LandingPage/LandingPage"
+import MisionPage from "./pages/MisionPage/MisionPage"
+import VisionPage from "./pages/VisionPage/VisionPage"
+import ObjectivesPage from "./pages/ObjectivesPage/ObjectivesPage"
 
 
 
 function App() {
   return (
     <>
-      <Navbar/>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
+        <Route path="/mision" element={<MisionPage/>}/>
+        <Route path="/vision" element={<VisionPage/>}/>
+        <Route path="/objetivos" element={<ObjectivesPage/>}/>
       </Routes>
       <Footer/>
     </>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import Hero from "../../components/Hero/Hero";
 import Title from "../../components/Title/Title";
 import Card from "../../components/Card/Card";
 import Information from "../../components/Information/Information";
 import Reviews from "../../components/Reviews/Reviews";
 import DropdownMenu from "../../components/DropdownMenu/DropdownMenu";
+import Navbar from "../../components/NavBar/Navbar";
 
 
 const LandingPage = () => {
     return (
         <div className="App">
+        <Navbar/>
         <Hero/>
         <section className="tours-section">
           <Title title="Tours disponibles" description = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam placeat sint consequuntur officiis alias dolore. Numquam debitis vel amet odio unde vitae velit repellendus. Porro adipisci enim eveniet laborum mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat veniam repudiandae provident architecto nihil consequuntur a cumque cupiditate ad. Corporis veniam rerum velit pariatur porro qui eos quasi suscipit eius?"/>
