dfdae639131e8726283804ae588f5ee4bf733b5d
andrescarcia <49753315+andrescarcia@users.noreply.github.com>
2023-06-22 21:57:51 -0400

    Calendario

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,33 +1,67 @@
-import Subtitle from "../../components/Subtitle/Subtitle";
-import "./CalendarPage.css"
+// import Subtitle from "../../components/Subtitle/Subtitle";
+// import "./CalendarPage.css"
 
+
+// const CalendarPage = () => {
+//     return (
+//         <div className="App">
+//             <header className="back-header">
+//                 <i className="fa-solid fa-arrow-left"></i>
+//             </header>
+//             <div className="calendar-section">
+//                 <Subtitle subtitle="Calendario de eventos" />
+//                 <div className="calendar-container">
+//                 </div>
+//             </div>
+//             <div className="navegation">
+//                 <div className="box">
+//                     <p>Nada en tu agenda?</p>
+//                     <button className="blue-btn">Registrarse ahora</button>
+//                 </div>
+//                 <div className="box">
+//                     <p></p>
+//                 </div>
+//                 <div className="box">
+//                     <p>Todavía no estas seguro?</p>
+//                     <button className="blue-btn">Revisar Tours</button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default CalendarPage
+import Subtitle from "../../components/Subtitle/Subtitle";
+import "./CalendarPage.css";
+import { Calender } from '../../../src/components/Calendar/Calendar';
 
 const CalendarPage = () => {
-    return (
-        <div className="App">
-            <header className="back-header">
-                <i className="fa-solid fa-arrow-left"></i>
-            </header>
-            <div className="calendar-section">
-                <Subtitle subtitle="Calendario de eventos" />
-                <div className="calendar-container">
-                </div>
-            </div>
-            <div className="navegation">
-                <div className="box">
-                    <p>Nada en tu agenda?</p>
-                    <button className="blue-btn">Registrarse ahora</button>
-                </div>
-                <div className="box">
-                    <p></p>
-                </div>
-                <div className="box">
-                    <p>Todavía no estas seguro?</p>
-                    <button className="blue-btn">Revisar Tours</button>
-                </div>
-            </div>
+  return (
+    <div className="App">
+      <header className="back-header">
+        <i className="fa-solid fa-arrow-left"></i>
+      </header>
+      <div className="calendar-section">
+        <Subtitle subtitle="Calendario de eventos" />
+        <div className="calendar-container">
+          <Calender />
         </div>
-    )
-}
+      </div>
+      <div className="navegation">
+        <div className="box">
+          <p>Nada en tu agenda?</p>
+          <button className="blue-btn">Registrarse ahora</button>
+        </div>
+        <div className="box">
+          <p></p>
+        </div>
+        <div className="box">
+          <p>Todavía no estás seguro?</p>
+          <button className="blue-btn">Revisar Tours</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CalendarPage
+export default CalendarPage;
