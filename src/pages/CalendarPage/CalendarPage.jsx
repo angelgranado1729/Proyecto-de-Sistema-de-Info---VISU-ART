32cbdb2a90c15320afa9cbd9aea380b934580170
André Scarcia <andre.scarcia@correo.unimet.edu.ve>
2023-07-01 14:17:22 -0400

    comentario del codigo que puede funcionar

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -68,3 +68,84 @@ const Calendario = () => {
 };
 
 export default Calendario;
+
+
+// import { useState, useEffect } from 'react';
+// import { Calendar, momentLocalizer } from 'react-big-calendar';
+// import moment from 'moment';
+// import { collection, query, onSnapshot } from 'firebase/firestore';
+// import { db } from '../../firebase/firebase-config';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import "./CalendarPage.css";
+// import Subtitle from "../../components/Subtitle/Subtitle";
+
+// const localizer = momentLocalizer(moment);
+
+// const Calendario = () => {
+//   const [events, setEvents] = useState([]);
+
+//   useEffect(() => {
+//     const q = query(collection(db, 'Tours'));
+//     const unsubscribe = onSnapshot(q, (querySnapshot) => {
+//       let eventsArray = [];
+//       querySnapshot.forEach((doc) => {
+//         const data = doc.data();
+//         data.fecha.forEach((date) => {
+//           eventsArray.push({
+//             title: data.nombre,
+//             start: moment(date, 'DD/MM/YYYY').toDate(),
+//             end: moment(date, 'DD/MM/YYYY').toDate(),
+//             allDay: true,
+//           });
+//         });
+//       });
+//       setEvents(eventsArray);
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   const handleSelectEvent = (event) => {
+//     // Implementa la lógica para redirigir al usuario al evento correspondiente
+//     console.log(event);
+//   };
+
+//   const handleSelectSlot = (slotInfo) => {
+//     // Muestra el texto completo del evento
+//     alert(slotInfo.start);
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="calendar-section">
+//         <header className="back-header">
+//           <i className="fa-solid fa-arrow-left"></i>
+//         </header>
+//         <Subtitle subtitle="Calendario de eventos" />
+//         <div className="calendar-container">
+//           <Calendar
+//             localizer={localizer}
+//             events={events}
+//             startAccessor="start"
+//             endAccessor="end"
+//             onSelectEvent={handleSelectEvent}
+//             onSelectSlot={handleSelectSlot}
+//           />
+//           <div className="calendar-navegation">
+//             <div className="calendar-box">
+//               <p>Nada en tu agenda?</p>
+//               <button className="blue-btn">Registrarse ahora</button>
+//             </div>
+//             <div className="calendar-box"><p></p></div>
+//             <div className="calendar-box">
+//               <p>Todavía no estás seguro?</p>
+//               <button className="blue-btn">Revisar Tours</button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Calendario;
