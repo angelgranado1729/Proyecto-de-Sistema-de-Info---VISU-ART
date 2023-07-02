ab3877d0335941ecb8ed2c39e23eb367ed1e8fee
SeaCast <s.castillo2020@correo.unimet.edu.ve>
2023-07-02 15:18:10 -0400

    Merge con Reserves 2

--- src/App.jsx
+++ src/App.jsx
@@ -66,312 +66,315 @@ import {
   RESERVE_LOOK_URL
 } from "./constants/urls";
 import "./App.css";
+import { TourContextProvider } from "./contexts/TourContext";
 
 function App() {
   return (
     <>
       <UserContextProvider>
-        <Routes>
-          <Route
-            path={HOME_URL}
-            element={<LandingPage />}
-          />
+        <TourContextProvider>
+          <Routes>
+            <Route
+              path={HOME_URL}
+              element={<LandingPage />}
+            />
 
-          <Route
-            path={ERROR_404}
-            element={
-              <NotFoundPage />
-            }
-          />
+            <Route
+              path={ERROR_404}
+              element={
+                <NotFoundPage />
+              }
+            />
 
-          <Route
-            path={VISION_URL}
-            element={
-              <VisionPage />
-            }
-          />
+            <Route
+              path={VISION_URL}
+              element={
+                <VisionPage />
+              }
+            />
 
-          <Route
-            path={MISION_URL}
-            element={
-              <MisionPage />
-            }
-          />
+            <Route
+              path={MISION_URL}
+              element={
+                <MisionPage />
+              }
+            />
 
-          <Route
-            path={OBJECTIVES_URL}
-            element={
-              <ObjectivesPage />
-            }
-          />
+            <Route
+              path={OBJECTIVES_URL}
+              element={
+                <ObjectivesPage />
+              }
+            />
 
-          <Route
-            path={CONTACT_URL}
-            element={
-              <ContactPage />
-            }
-          />
+            <Route
+              path={CONTACT_URL}
+              element={
+                <ContactPage />
+              }
+            />
 
-          <Route
-            path={CALENDAR_URL}
-            element={
-              <CalendarPage />
-            }
-          />
+            <Route
+              path={CALENDAR_URL}
+              element={
+                <CalendarPage />
+              }
+            />
 
-          <Route
-            path={RESERVE_LOOK_URL}
-            element={
-              <PrivateRoute>
-                <LookReserve />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path={RESERVE_URL}
-            element={
-              <PrivateRoute>
-                <ReservePage />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path={RESERVE_URL}
-            element={
-              <PrivateRoute>
-                <ReservePage />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path={FEEDBACK_URL }
-            element={
-              <PrivateRoute>
-                <FeedbackPage />
+            <Route
+              path={RESERVE_LOOK_URL}
+              element={
+                <PrivateRoute>
+                  <LookReserve />
                 </PrivateRoute>
-            }
-          />      
+              }
+            />
 
-          <Route
-            path={REGISTER_URL}
-            element={
-              <PublicRoute>
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
+            <Route
+              path={RESERVE_URL}
+              element={
+                <PrivateRoute>
+                  <ReservePage />
+                </PrivateRoute>
+              }
+            />
 
-          
-       
+            <Route
+              path={RESERVE_URL}
+              element={
+                <PrivateRoute>
+                  <ReservePage />
+                </PrivateRoute>
+              }
+            />
 
-          <Route
-            path={REGISTER_FORM_URL}
-            element={
-              <PublicRoute>
-                <RegisterFormPage />
-              </PublicRoute>
-            }
-          />
+            <Route
+              path={FEEDBACK_URL }
+              element={
+                <PrivateRoute>
+                  <FeedbackPage />
+                  </PrivateRoute>
+              }
+            />      
 
-          <Route
-            path={ERROR_404}
-            element={
-              <NotFoundPage />
-            }
-          />
+            <Route
+              path={REGISTER_URL}
+              element={
+                <PublicRoute>
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
 
-          {/* Admin Pages */}
-          <Route
-            path={ADMIN_URL}
-            element={
-              <PrivateRoute>
-                <AdminPage />
-              </PrivateRoute>
-            }
-          />
+            
+        
 
-          <Route
-            path={ADMIN_URL_CONTACT}
-            element={
-              <PrivateRoute>
-                <AdminPageContact />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={REGISTER_FORM_URL}
+              element={
+                <PublicRoute>
+                  <RegisterFormPage />
+                </PublicRoute>
+              }
+            />
 
+            <Route
+              path={ERROR_404}
+              element={
+                <NotFoundPage />
+              }
+            />
 
-          {/* Login and register */}
-          <Route
-            path={LOGIN_URL}
-            element={
-              <PublicRoute>
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
+            {/* Admin Pages */}
+            <Route
+              path={ADMIN_URL}
+              element={
+                <PrivateRoute>
+                  <AdminPage />
+                </PrivateRoute>
+              }
+            />
 
-          <Route
-            path={FORGOT_PASSWORD_URL}
-            element={
-              <ForgotPasswordPage />
-            }
-          />
-
-          <Route
-            path={RESET_PASSWORD_URL}
-            element={
-              <ResetPasswordPage />
-            }
-          />
-
-          <Route
-            path={REGISTER_URL}
-            element={
-              <PublicRoute>
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
-
-          <Route
-            path={REGISTER_FORM_URL}
-            element={
-              <PublicRoute>
-                <RegisterFormPage />
-              </PublicRoute>
-            }
-          />
+            <Route
+              path={ADMIN_URL_CONTACT}
+              element={
+                <PrivateRoute>
+                  <AdminPageContact />
+                </PrivateRoute>
+              }
+            />
 
 
+            {/* Login and register */}
+            <Route
+              path={LOGIN_URL}
+              element={
+                <PublicRoute>
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
 
-          <Route
-            path={ADMIN_RESERVE_URL}
-            element={
-              <PrivateRoute>
-                <ReserveAdminPage />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={FORGOT_PASSWORD_URL}
+              element={
+                <ForgotPasswordPage />
+              }
+            />
 
-          <Route
-            path={ADMIN_RESERVE_URL}
-            element={
-              <PrivateRoute>
-                <ReserveAdminPage />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={RESET_PASSWORD_URL}
+              element={
+                <ResetPasswordPage />
+              }
+            />
 
-          <Route
-            path={ADMIN_EDIT_OBRAS_URL}
-            element={
-              <PrivateRoute>
-                <ArtEdit />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={REGISTER_URL}
+              element={
+                <PublicRoute>
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
 
-          <Route
-            path={ADMIN_TOURS_CALENDAR_URL}
-            element={
-              <PrivateRoute>
-                <TourEditFechas />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path={ADMIN_CREATE_OBRAS_URL}
-            element={
-              <PrivateRoute>
-                <CreateArt />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path={ADMIN_OBRAS_URL}
-            element={
-              <PrivateRoute>
-                 <ArtAdmin/>
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path={ADMIN_TOURS_EDIT_URL}
-            element={
-              <PrivateRoute>
-                <TourEdit />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={REGISTER_FORM_URL}
+              element={
+                <PublicRoute>
+                  <RegisterFormPage />
+                </PublicRoute>
+              }
+            />
 
 
-          <Route
-            path={ADMIN_TOURS_URL}
-            element={
-              <PrivateRoute>
-                <TourAdmin />
-              </PrivateRoute>
-            }
-          />
 
-          <Route
-            path={ADMIN_TOUR_CREATE_URL}
-            element={
-              <PrivateRoute>
-                <CreateTour />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={ADMIN_RESERVE_URL}
+              element={
+                <PrivateRoute>
+                  <ReserveAdminPage />
+                </PrivateRoute>
+              }
+            />
 
-          <Route
-            path={ADMIN_TOURS_EDIT_ART}
-            element={
-              <PrivateRoute>
-                <TourEditObras />
-              </PrivateRoute>
+            <Route
+              path={ADMIN_RESERVE_URL}
+              element={
+                <PrivateRoute>
+                  <ReserveAdminPage />
+                </PrivateRoute>
+              }
+            />
 
-            }
-          />
+            <Route
+              path={ADMIN_EDIT_OBRAS_URL}
+              element={
+                <PrivateRoute>
+                  <ArtEdit />
+                </PrivateRoute>
+              }
+            />
 
-          {/* User Pages */}
-          <Route
-            path={USER_PROFILE_URL}
-            element={
-              <PrivateRoute>
+            <Route
+              path={ADMIN_TOURS_CALENDAR_URL}
+              element={
+                <PrivateRoute>
+                  <TourEditFechas />
+                </PrivateRoute>
+              }
+            />
 
-                <UserProfilePage />
-              </PrivateRoute>
+            <Route
+              path={ADMIN_CREATE_OBRAS_URL}
+              element={
+                <PrivateRoute>
+                  <CreateArt />
+                </PrivateRoute>
+              }
+            />
 
-            }
-          />
+            <Route
+              path={ADMIN_OBRAS_URL}
+              element={
+                <PrivateRoute>
+                  <ArtAdmin/>
+                </PrivateRoute>
+              }
+            />
 
-          <Route
-            path={USER_PROFILE_EDIT_URL}
-            element={
-              <PrivateRoute>
+            <Route
+              path={ADMIN_TOURS_EDIT_URL}
+              element={
+                <PrivateRoute>
+                  <TourEdit />
+                </PrivateRoute>
+              }
+            />
 
-                <EditProfilePage />
-              </PrivateRoute>
 
-            }
-          />
+            <Route
+              path={ADMIN_TOURS_URL}
+              element={
+                <PrivateRoute>
+                  <TourAdmin />
+                </PrivateRoute>
+              }
+            />
 
-          <Route
-            path={USER_PROFILE_EDIT_URL}
-            element={
-              <PrivateRoute>
-                <EditProfilePage />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path={ADMIN_TOUR_CREATE_URL}
+              element={
+                <PrivateRoute>
+                  <CreateTour />
+                </PrivateRoute>
+              }
+            />
 
-        </Routes >
+            <Route
+              path={ADMIN_TOURS_EDIT_ART}
+              element={
+                <PrivateRoute>
+                  <TourEditObras />
+                </PrivateRoute>
+
+              }
+            />
+
+            {/* User Pages */}
+            <Route
+              path={USER_PROFILE_URL}
+              element={
+                <PrivateRoute>
+
+                  <UserProfilePage />
+                </PrivateRoute>
+
+              }
+            />
+
+            <Route
+              path={USER_PROFILE_EDIT_URL}
+              element={
+                <PrivateRoute>
+
+                  <EditProfilePage />
+                </PrivateRoute>
+
+              }
+            />
+
+            <Route
+              path={USER_PROFILE_EDIT_URL}
+              element={
+                <PrivateRoute>
+                  <EditProfilePage />
+                </PrivateRoute>
+              }
+            />
+
+          </Routes >
+        </TourContextProvider>
       </UserContextProvider >
       <Footer />
     </>
--- src/pages/ReservePage/ReservePage.jsx
+++ src/pages/ReservePage/ReservePage.jsx
@@ -1,54 +1,157 @@
-import React, { useEffect } from 'react'
-import styles from './ReservePage.css'
-import FormItem from '../../components/FormItem/FormItem'
-import Title from '../../components/Title/Title'
+import React, { useContext, useEffect, useState } from 'react'
+import styles from './ReservePage.module.css'
 import Subtitle from '../../components/Subtitle/Subtitle'
-import DropdownMenu from '../../components/DropdownMenu/DropdownMenu'
-import { userTour } from '../../contexts/TourContext'
+import { TourContext, useTour } from '../../contexts/TourContext'
 import { useTourList } from '../../hooks/useTourList'
 import DropdownTour from '../../components/DropdownTour/DropdownTour'
+import DropdownDates from '../../components/DropdownDates/DropdownDates'
+import { useUserContext } from '../../contexts/UserContext'
+import { createReserve } from '../../firebase/reservaciones'
+import { Link, useNavigate } from 'react-router-dom'
+import { CALENDAR_URL, HOME_URL } from '../../constants/urls'
+import { CustomToast } from '../../components/CustomToast/CustomToast'
+import { ArrowLeft } from 'react-bootstrap-icons'
 
 function ReservePage() {
   const { tourList, listLoading, getTourList } = useTourList();
+  const { tour, tourId, changeId, changeTour, resetTour} = useContext(TourContext);
+  const {user} = useUserContext();
+  const navigate = useNavigate();
+  const [showToast, setShowToast] = useState(false);
+  const [dateEmpty, setDateEmpty] = useState(false);
+  const [tourEmpty, setTourEmpty] = useState(false);
+  const [duplicateReserve, setDuplicateReserve] = useState(false);
 
     useEffect( () => {
         //Fetch de lista de tours
         getTourList();
     }, [])
 
+/***
+ * La pagina de reservas es una vista privada que requiere de una sesion activa para acceder
+ * toma los datos de tour e id del context, que deberian iniciar como null y vacio inicialmente,
+ * tambien realiza una llamada al hook del listado de tours para adquirir la lista de tours necesaria
+ * para renderizar la version de Dropdown menu para tours, si se encuentra cargando, se mostrara 
+ * un texto que indica el estado, si no, renderiza el componente de DropdownTour y DropdownDates, donde habra un listado
+ * con los tours actuales en la coleccion, una vez el usuario seleccione un tour, DropdownDates se actualizara con las
+ * fechas que tiene dicho tour, al salir de la pagina se debera reiniciar el context de tours (reserva) a menos que 
+ * la vista sea el siguiente paso de la reserva. El usuario no debe poder ir al siguiente paso si no ha seleccionado una fecha.
+ */
 
-    console.log(tourList);
+function finishReserve(reserveItem){
+    createReserve(reserveItem, user)
+}
+
+function reserveCheck(){
+    if (tour) {
+
+        if (tour.chosenFecha) {
+            const reserveItem = {
+                fecha: tour.chosenFecha,
+                tour_id: tourId.id,
+                user_id: user.id 
+            }
+
+            let alredyReserved = false;
+            user.reservations.forEach(item => {if (reserveItem.tour_id == item.tour_id && reserveItem.fecha == item.fecha){
+                alredyReserved = true;
+            }})
+
+            if (alredyReserved) {
+                setShowToast(true);
+                setTourEmpty(false);
+                setDateEmpty(false);
+                setDuplicateReserve(true)
+            }
+
+            else{
+                finishReserve(reserveItem);
+                resetTour();
+                navigate(HOME_URL);
+            }
+
+
+        } else {
+            setShowToast(true);
+            setTourEmpty(false);
+            setDateEmpty(true);
+        }
+
+    } else{
+        setShowToast(true);
+        setTourEmpty(true);
+        setDateEmpty(false);
+    }
+}
+
   return (
     <div className="App">
-        <header className="back-header">
-            <i className="fa-solid fa-arrow-left"></i>
-        </header>
+        <div className={styles.backbutton}>
+            <Link to={HOME_URL}>
+                <ArrowLeft size={40} color="#000000" />
+            </Link>
+        </div>
 
-    <div className='centeredArea'>
+    <div className={styles.centeredArea}>
         <Subtitle subtitle = "Reservar tu Tour"/>
-        <p className='reserveSubSubtitle'>Terminos y condiciones de la reservacion bla bla hola</p>
+        <p className={styles.reserveSubSubtitle}>Terminos y condiciones de la reservacion bla bla hola</p>
         
-        <section className='rForm'>
-            <div className='inpArea'>
+        <section className={styles.rForm}>
+            <div className={styles.inpArea}>
                 {listLoading &&(<h1>Cargando...</h1>)}
 
                 {!listLoading && tourList &&(
-                    <DropdownTour tours = {tourList}/>
+                    <>
+                        <DropdownTour tours = {tourList}/>
+                        <DropdownDates/>
+
+                        {showToast && tourEmpty &&(
+                            <CustomToast
+                            typeToast="error"
+                            title="¡Error!"
+                            message="Para seguir, debe seleccionar un tour y una fecha"
+                            time={5000}
+                            />
+                        )}
+
+                        {showToast && dateEmpty &&(
+                            <CustomToast
+                            typeToast="error"
+                            title="¡Error!"
+                            message="Para seguir, debe seleccionar una fecha"
+                            time={5000}
+                            />
+                        )}
+
+                        {showToast && duplicateReserve &&(
+                            <CustomToast
+                            typeToast="error"
+                            title="¡Error!"
+                            message="Ya se ha realizado una reserva para este tour en la fecha ingresada"
+                            time={5000}
+                            />
+                        )}
+                        
+                    </>
                 )}
 
             </div>
 
-            <div className='textArea'>
+            <div className={styles.textArea}>
                 <p> Aqui podrás explorar y disfrutar de las obras de arte que residen en nuestra institución. Nuestra misión es promover el arte y la cultura en nuestra comunidad, y para ello ofrecemos una variedad de tours que te permitirán conocer y apreciar nuestras colecciones de manera única e inspiradora. Únete a nosotros en esta experiencia cultural y descubre el valor y la belleza del arte en nuestro campus universitario</p>
             </div>
         </section>
     </div>
 
-        <section className='buttonArea'>
-            <button className='blue-btn'>Ver Calendario</button>
-            <button className='blue-btn'>Ver otros tours</button>
-            <button className='orange-btn'>Siguiente</button>
+        <section className={styles.buttonArea}>
+            <button className={styles.bluebtn} onClick={() => navigate(CALENDAR_URL)}>Ver Calendario</button>
+            <button className={styles.bluebtn} onClick={() => navigate(HOME_URL)}>Ver tours</button>
+            <button className={styles.orangebtn} onClick={() => {reserveCheck()}}>Siguiente</button>
         </section>
+
+        <div className={styles.reservedecoration1}></div>
+        <div className={styles.reservedecoration2}></div>
+        <div className={styles.reservedecoration3}></div>
 
     </div>
 
@@ -56,3 +159,36 @@ function ReservePage() {
 }
 
 export default ReservePage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Creo que deberia buscar mejores nombres para las variables*/
